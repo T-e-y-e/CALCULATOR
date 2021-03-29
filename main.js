f8cf6ae9b7e583d1d4785b129cae5cdde63c592f
T-connect <63002972+T-connect@users.noreply.github.com>
2021-03-29 14:23:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+let screen = document.getElementById("display");
+
+function insert(num){
+        screen.value =
+            screen.value + num;
+    
+} 
+
+
+function equalTo() {
+    if (screen) {
+        screen.value = eval(
+        screen.value
+        );
+    }
+    
+}
+
+function clean(){
+    screen.value = ""
+}
+
+function back(){
+    let del = document.getElementById("display").value;
+    document.getElementById("display").value = del.substring(0, del.length-1)
+}
+
+
+
+
+
+
+
